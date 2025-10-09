20f8264af39bf0bf50f00f3245d9da67adddf967
Salem <salem.djekoun@gmail.com>
2025-10-09 16:53:29 +0200

    [wip] ... Integration of React with PNPM preconfiguration.

---
+++ app/source/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Bienvenue sur la page QAP</h1>
+      <p>Ceci est une page HTML simple.</p>
+      <small>version: 0.3.1</small>
+    </>
+  )
+}
+
+export default App
