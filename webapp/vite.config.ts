cfab55e1e7748213c35792db7db81c3323afcff5
Salem <salem.djekoun@gmail.com>
2025-10-13 15:27:30 +0200

    update Vite config for correct build and alias resolution

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -7,7 +7,18 @@ import path from 'path';
 export default defineConfig({
 	plugins: [ react() ],
   	root: 'source',
+	base: './',
 	resolve: {
   		alias: { '@libs': path.resolve(__dirname, '../libs') }
-	}
+	},
+	build: {
+    	outDir: path.resolve(__dirname, '.dist'),
+    	emptyOutDir: true,
+    	rollupOptions: {
+      		input: [ 
+				'@libs/react-material',
+				'source/index.html' 
+			]
+    	}
+  	}
 })
