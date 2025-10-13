b4f4ad757812f39c6247c23443a6d830d959217e
Salem <salem.djekoun@gmail.com>
2025-10-13 16:28:54 +0200

    Use the correct tree structure

--- test/component/libs/react-materials/jest.config.ts
+++ test/component/libs/react-materials/jest.config.ts
@@ -3,6 +3,6 @@ export default {
     testMatch: [ '**/*.test.{ts,tsx}' ],
     transform: { '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
     testEnvironment: 'jsdom',
-    moduleNameMapper: { '^@libs/(.*)$': '<rootDir>/../../../libs/$1/source' },
+    moduleNameMapper: { '^@libs/(.*)$': '<rootDir>/../../../../libs/$1/source' },
     setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 }
