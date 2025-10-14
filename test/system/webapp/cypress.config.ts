b8537091b2500f09aefeac842554b84d9891b1cc
Salem <salem.djekoun@gmail.com>
2025-10-14 12:51:57 +0200

    Allow the use of a dynamic URL

--- test/system/webapp/cypress.config.ts
+++ test/system/webapp/cypress.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+
+const baseUrl = process.env.CYPRESS_BASE_URL || 'http://localhost:5173';
 
 export default defineConfig({
 
   e2e: {
-        baseUrl: 'http://localhost:5173',
+        baseUrl,
         specPattern: '**/*.system.spec.{js,jsx,ts,tsx}',
         supportFile: false
     }
