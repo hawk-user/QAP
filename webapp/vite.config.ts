503a9c6397fbad470ed58d8905b3e6a3c616c474
Salem <salem.djekoun@gmail.com>
2025-10-15 15:19:18 +0200

    Node.js built-in modules should be imported using the 'node:' protocol

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import path from 'node:path';
 
 // 💡 Vite config reference → https://vite.dev/config/
 
