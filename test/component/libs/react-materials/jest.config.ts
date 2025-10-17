a347c62aaf631e11f64f31bd90539eba89de04fc
Salem <salem.djekoun@gmail.com>
2025-10-17 12:27:01 +0200

    ...debug

---
+++ test/component/libs/react-materials/jest.config.ts
@@ -0,0 +1,22 @@
+import path from 'node:path';
+
+const ROOT_DIR = path.resolve(__dirname, '../../../..');
+
+export default {
+        rootDir: ROOT_DIR,
+        roots: [
+            '<rootDir>/test/component/libs/react-materials',
+            '<rootDir>/libs/react-material/source',
+        ],
+        testMatch: ['**/*.test.{ts,tsx}'],
+        testEnvironment: 'jsdom',
+        transform: {
+            '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+        },
+        setupFilesAfterEnv: [
+            '<rootDir>/test/component/libs/react-materials/setupTests.ts',
+        ],
+        moduleNameMapper: {
+            '^@libs/react-material$': '<rootDir>/libs/react-material/source',
+        }
+}
--- test/component/libs/react-materials/test-configs/jest.component.config.ts
+++
@@ -1,3 +0,0 @@
-import { baseConfig } from './jest.base.config'
-
-export default { ...baseConfig }
