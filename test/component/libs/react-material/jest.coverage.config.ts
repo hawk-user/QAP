301947a3080406e4454c0818574f103d06055d75
Salem <salem.djekoun@gmail.com>
2025-10-23 10:10:24 +0200

    fix: modify the error in the folder name that blocked some workflows

--- test/component/libs/react-material/jest.coverage.config.ts
+++ test/component/libs/react-material/jest.coverage.config.ts
@@ -2,14 +2,14 @@ import { Config } from 'jest';
 
 const config: Config = {
       rootDir: '../../../../libs/react-material',
-      roots: [ '<rootDir>', '<rootDir>/../../test/component/libs/react-materials/' ],
+      roots: [ '<rootDir>', '<rootDir>/../../test/component/libs/react-material/' ],
       testMatch: ['**/*.test.{ts,tsx}'],
       testEnvironment: 'jsdom',
       transform: {
         '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
       },
       setupFilesAfterEnv: [
-        '<rootDir>/../../test/component/libs/react-materials/setupTests.ts',
+        '<rootDir>/../../test/component/libs/react-material/setupTests.ts',
       ],
       moduleNameMapper: {
         '^@libs/react-material(.*)$': '<rootDir>/source$1'
@@ -18,7 +18,7 @@ const config: Config = {
         '!**/*.d.ts',
         '!**/node_modules/**',
       ],
-      coverageDirectory: '<rootDir>/../../test/component/libs/react-materials/coverage',
+      coverageDirectory: '<rootDir>/../../test/component/libs/react-material/coverage',
       coverageReporters: ['text', 'lcov'],
 
 };
